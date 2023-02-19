9393547f9e5054972b57ee874fa7ece17d7363e2
Zong Yuan Lee <zy.lee@nettium.net>
2023-02-20 00:25:11 +0800

    first commit

---
+++ asp.netCore/Data/aspnetCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using asp.netCore.Models;
+
+namespace asp.netCore.Data
+{
+    public class aspnetCoreContext : DbContext
+    {
+        public aspnetCoreContext (DbContextOptions<aspnetCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<asp.netCore.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ asp.netCore/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using asp.netCore.Data;
+using asp.netCore.Models;
+
+namespace asp.netCore.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly asp.netCore.Data.aspnetCoreContext _context;
+
+        public DetailsModel(asp.netCore.Data.aspnetCoreContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
